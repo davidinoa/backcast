11ff7ce90f0b8203091dcbfcd941ef49d9527f08
David Inoa <davidinoa@gmail.com>
2017-12-12 13:50:22 -0500

    Pass exampleVideoData into collection and render VideoListView

--- src/views/app.js
+++ src/views/app.js
@@ -3,13 +3,17 @@ var AppView = Backbone.View.extend({
   el: '#app',
 
   initialize: function() {
-    this.videos = new Videos();
+    this.videos = new Videos(window.exampleVideoData);
     this.render();
+    console.log(this.videos);
+    return this;
   },
 
 
   render: function() {
     this.$el.html(this.template());
+
+    new VideoListView({collection: this.videos, el: this.$('.list')});
 
     return this;
   },
